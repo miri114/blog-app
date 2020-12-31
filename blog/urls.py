dae3a4659fe48396ed042c7e7b061e807156863b
Miri <delomiremini@gmail.com>
2020-12-31 11:29:21 +0100

    Adding a lot of other functionalities

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path
-from .views import BlogListView ,BlogDetailView
+
+from .views import(
+        BlogListView ,
+        BlogDetailView,
+        BlogCreateView,
+        BlogUpdateView,
+        BlogDeleteView,
+) 
+
 
 urlpatterns = [
+    
+    path('post/<int:pk>/delete/',
+            BlogDeleteView.as_view() , name='post_delete'),
+
+    path('post/<int:pk>/edit/',
+            BlogUpdateView.as_view(), name='post_edit'),
+
+    path('post/new/' , BlogCreateView.as_view(), name='post_new'),               #new 2
 
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), #new added
 
-    path('' , BlogListView.as_view() , name='home')
-
-    
+    path('' , BlogListView.as_view(), name='home')    
 ]
